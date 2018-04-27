57dc9116f45d3065a7937e8e658a6d57357510bb
Can <Jayteare@users.noreply.github.com>
2018-04-27 12:12:24 -0700

    Update 462input_handler.php

--- Php/462input_handler.php
+++ Php/462input_handler.php
@@ -6,7 +6,7 @@
     $username=$_POST['username'];
     $password=$_POST['password'];
     //Query to check if User/Pass combo is in Employee table
-    $query = "SELECT COUNT(*), username, password, fname, lname FROM (SELECT username, password, fname, lname FROM employees WHERE username = '".$username."' && password = '".$password."') AS x";
+    $query = "SELECT COUNT(*), username, password, fname, lname, company_pin FROM (SELECT username, password, fname, lname, company_pin FROM employees WHERE username = '".$username."' && password = '".$password."') AS x";
     $stmt = $db->prepare($query);
     $stmt->execute();
     $result = $stmt->fetchAll();
@@ -16,32 +16,34 @@
     $manstmt->execute();
     $manresult = $manstmt->fetchAll();
     /*
-    Loop checks query results to determine if user in the Employee table, if
-    not checks results of Manager table, redirecting to appropriate homepage.
-    If in neither table, user is redirected to login screen.
+      Loop checks query results to determine if user in the Employee table, if
+      not checks results of Manager table, redirecting to appropriate homepage.
+      If in neither table, user is redirected to login screen.
     */
     foreach ($result as $row){
       if($row['COUNT(*)'] > 0){
         $_SESSION['cur_user'] = $username;
         $_SESSION['fname'] = $row['fname'];
         $_SESSION['lname'] = $row['lname'];
-        header('Location:Php/employee_homepage.html.php');
+        $_SESSION['company_id'] = $row['company_pin'];
+        header('Location:http://localhost/462Project/employee_newshift.html.php');
       }else{
         foreach($manresult as $manrow){
           if($manrow['COUNT(*)'] > 0){
             $_SESSION['cur_user'] = $username;
             $_SESSION['fname'] = $manrow['fname'];
             $_SESSION['lname'] = $manrow['lname'];
-            header('Location:Php/manager_homepage.html.php');
+            $_SESSION['lname'] = $row['lname'];
+            header('manager_homepage.html.php');
           }else{
-            header('Location:Php/index.html.php');
+            header('index.html.php');
           }
         }
       }
     }
   }
 
-  //Activated when "Create New Employee Account" is pressed on index.html.php
+  //Activated when "Submit" button is pressed on create_new_employee.html.php
   if(isset($_POST['create_employee_submit'])){
 	  $_SESSION['new_employee_username']=$_POST['new_employee_username'];
 	  $_SESSION['new_employee_password']=$_POST['new_employee_password'];
@@ -51,9 +53,9 @@
 	  $_SESSION['new_employee_email']=$_POST['new_employee_email'];
     $_SESSION['new_employee_phone']=$_POST['new_employee_phone'];
     /*
-    Query determines if username provided is already in database, if
-    so currently submitted values are stored in $_SESSION to be displayed on
-    create_new_employee.html.php
+      Query determines if username provided is already in database, if
+      so currently submitted values are stored in $_SESSION to be displayed on
+      create_new_employee.html.php
     */
     $checkquery = "SELECT COUNT(*) FROM (SELECT username FROM employees WHERE username = '".$_SESSION['new_employee_username']."' AND company_pin = '".$_SESSION['new_employee_pin']."') AS x";
     $checkstmt = $db->prepare($checkquery);
@@ -63,9 +65,9 @@
     foreach ($checkresult as $checkrow) {
       if($checkrow['COUNT(*)'] < 1){
         /*
-        Query determines if PIN provided matches an existing manager's personal
-        PIN. If PIN doesn match currently submitted values are stored in
-        $_SESSION to be displayed on create_new_employee.html.php
+          Query determines if PIN provided matches an existing manager's personal
+          PIN. If PIN doesn't match, currently submitted values are stored in
+          $_SESSION to be displayed on create_new_employee.html.php
         */
         $query = "SELECT COUNT(*) FROM (SELECT idcompany FROM company WHERE idcompany = '".$_SESSION['new_employee_pin']."') AS x";
         $stmt = $db->prepare($query);
@@ -84,19 +86,20 @@
             $_SESSION['new_employee_email']=null;
             $_SESSION['new_employee_phone']=null;
             $_SESSION['employeeCreateErrorMsg']=null;
-    	      header('Location:Php/index.html.php');
+    	      header('Location:http://localhost/462Project/index.html.php');
           }else{
             $_SESSION['employeeCreateErrorMsg']="Your PIN must be valid!";
-            header('Location:Php/create_new_employee.html.php');
+            header('Location:http://localhost/462Project/create_new_employee.html.php');
           }
         }
       }else{
         $_SESSION['employeeCreateErrorMsg']="Your Username must be unique!";
-        header('Location:Php/create_new_employee.html.php');
+        header('Location:http://localhost/462Project/create_new_employee.html.php');
       }
     }
   }
 
+  //Activated when "Submit" button is pressed on create_new_company.html.php
   if(isset($_POST['create_company_submit'])){
 	  $_SESSION['new_company_title']=$_POST['new_company_title'];
     $_SESSION['new_company_pin']=$_POST['new_company_pin'];
@@ -108,9 +111,9 @@
 	  $_SESSION['new_owner_email']=$_POST['new_owner_email'];
     $_SESSION['new_owner_phone']=$_POST['new_owner_phone'];
     /*
-    Query determines if company name provided is already in database, if
-    so currently an error message is stored in $_SESSION to be displayed on
-    create_new_company.html.php
+      Query determines if company name provided is already in database, if
+      so currently an error message is stored in $_SESSION to be displayed on
+      create_new_company.html.php
     */
     $checkquery = "SELECT COUNT(*) FROM (SELECT name FROM company WHERE name = '".$_SESSION['new_company_title']."') AS x";
     $checkstmt = $db->prepare($checkquery);
@@ -120,8 +123,8 @@
     foreach ($checkresult as $checkrow) {
       if($checkrow['COUNT(*)'] < 1){
         /*
-        Query determines if PIN provided already exists. If PIN isn't unique an error message is stored in
-        $_SESSION to be displayed on create_new_company.html.php
+          Query determines if PIN provided already exists. If PIN isn't unique an error message is stored in
+          $_SESSION to be displayed on create_new_company.html.php
         */
         $query = "SELECT COUNT(*) FROM (SELECT idcompany FROM company WHERE idcompany = '".$_SESSION['new_company_pin']."') AS x";
         $stmt = $db->prepare($query);
@@ -130,12 +133,15 @@
         foreach($result as $row){
           if($row['COUNT(*)'] > 0){
             $_SESSION['companyCreateErrorMsg']="Your company PIN must be unique!";
-            header('create_new_company.html.php');
+            header('Location:http://localhost/462Project/create_new_company.html.php');
           }else{
+            /*
+              These two queries create an entry in the company table and the owners table upon valid
+              submission in create_new_company.html.php
+            */
             $query = "INSERT INTO company VALUES ('".$_SESSION['new_company_pin']."', '".$_SESSION['new_company_title']."', '".$_SESSION['new_owner_fullname']."')";
             $stmt = $db->prepare($query);
             $stmt->execute();
-
             $query = "INSERT INTO owners VALUES ('".$_SESSION['new_owner_username']."', '".$_SESSION['new_owner_password']."', '".$_SESSION['new_owner_fname']."', '".$_SESSION['new_owner_lname']."', '".$_SESSION['new_owner_email']."', '".$_SESSION['new_owner_phone']."', '".$_SESSION['new_company_pin']."')";
             $stmt = $db->prepare($query);
             $stmt->execute();
@@ -149,13 +155,31 @@
             $_SESSION['new_owner_pin']=null;
             $_SESSION['new_owner_email']=null;
             $_SESSION['new_owner_phone']=null;
-            header('Location:index.html.php');
+            header('Location:http://localhost/462Project/index.html.php');
           }
         }
       }else{
         $_SESSION['companyCreateErrorMsg']="Your company name must be unique!";
-        header('create_new_company.html.php');
+        header('Location:http://localhost/462Project/create_new_company.html.php');
       }
     }
+  }
+
+  /*
+    Activated when the "Confirm" button is selected on enroll_confirm.html.php.  A query is prepared that takes the currently
+    selected "shiftid" from created_shifts and stores it along with the appropriate "company_id" and "username" in the queued_shifts
+    table.
+  */
+  if(isset($_POST['shift_enroll_confirmation'])){
+    $query = "INSERT INTO queued_shifts VALUES ('".$_SESSION['shift_enroll_id']."', '".$_SESSION['company_id']."', '".$_SESSION['cur_user']."')";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $_SESSION['shift_enroll_id'] = null;
+    header('Location:http://localhost/462Project/employee_newshift.html.php');
+  }
+  //Stores a selected datein $_SESSION for use in shiftlokup.php after employee_newshift.html.php is reached
+  if(isset($_POST['shift_date_submit'])){
+    $_SESSION['shift_date_display'] = $_POST['shift_date_range'];
+    header('Location:http://localhost/462Project/employee_newshift.html.php');
   }
 ?>
