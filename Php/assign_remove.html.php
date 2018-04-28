e855565704d6bb0c76ab51e5a52b029d366a8f0a
qigaoqigao <32440740+qigaoqigao@users.noreply.github.com>
2018-04-28 10:40:27 -0700

    Added manager_assign_shifts.html.php and assign_remove.html.php
    Uploaded manager assign shift functions

---
+++ Php/assign_remove.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+   Employee's Assigned 
+   <br>
+   <form action = "manager_homepage.html" >
+		<input type="submit" value="Main Menu">
+	</form>
+  </body>
+</html>
