b0b7fc4f16b6b4218e3c085e7b1a232f13f42ead
raymondlam0128 <raymondlam012892@gmail.com>
2018-04-28 11:14:42 -0700

    Update decline.php
    update host pass for database

--- Php/decline.php
+++ Php/decline.php
@@ -2,13 +2,13 @@
 
   $id = $_GET['id'];
 
-  $conn =  mysqli_connect('localhost', 'root', '', '462_schedule_project');
+  $conn =  mysqli_connect('us-cdbr-iron-east-05.cleardb.net:3306', 'b52e20d0f5da46', 'fc4f25b0', 'heroku_0188da0de4a5cfa');
 
   if(!$conn){
     die ("Connection failed:  " .mysqli_connect_error());
   }
 
-    $sql = "UPDATE prerequest SET prerequest.Status = 'Decline' where prerequest.ID=$id";
+    $sql = "UPDATE prerequest SET prerequest.Status = 'Declined' where prerequest.ID=$id";
     if (mysqli_query($conn, $sql)) {
       mysqli_close($conn);
 
