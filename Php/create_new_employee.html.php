548c10b8644fc6a7ce9b4ae6657daf1a606910a6
Can <Jayteare@users.noreply.github.com>
2018-04-28 05:23:41 -0700

    Update create_new_employee.html.php

--- Php/create_new_employee.html.php
+++ Php/create_new_employee.html.php
@@ -12,8 +12,44 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
 
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+			<!-- Animate.css -->
+	<link rel="stylesheet" href="css/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/css/bootstrap.css">
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Style -->
+	<link rel="stylesheet" href="css/css/style.css">
 	</head>
+	
+	<header role="banner" id="fh5co-header">
+	<div class="fluid-container">
+		<nav class="navbar navbar-default">
+			<div class="navbar-header">
+				<!-- Mobile Toggle Menu Button -->
+				<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+				<a class="" href="#">TaskerApp</a> 
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#" data-nav-section="home"><span>Home</span></a></li>
+					<li><a href="#" data-nav-section="services"><span>Services</span></a></li>
+					<li><a href="#" data-nav-section="team"><span>Team</span></a></li>
+					<li><a href="#" data-nav-section="faq"><span>FAQ</span></a></li>
+					<li class="call-to-action"><a class="sign-up" href="registration.html.php"><span>Register</span></a></li>
+					<li class="call-to-action"><a class="log-in" href="index.html.login"><span>Login</span></a></li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</header>
+
+	
 	<body>
 		<form action="462input_handler.php" method="post" style="border:1px solid #ccc">
 			<div class="row">
@@ -23,16 +59,19 @@
 				</div>
 				<div class="column">
 				
-					<label for="company-title"><b>Employee Username</b></label>
+					<label for="employee-username"><b>Employee Username</b></label>
 					<input type="text" placeholder="Enter Username" name="new_employee_username" required>
 					
-					<label for="company-pin"><b>Company PIN</b></label>
+					<label for="company-pin"><b>Employee Company PIN</b></label>
 					<input type="text" placeholder="Enter Company PIN" name="new_employee_pin" required>
 					
-					<label for="company-manager"><b>Employee Firstname</b></label>
+					<label for="employee-firstname"><b>Employee Firstname</b></label>
 					<input type="text" placeholder="Enter First Name" name="new_employee_fname" required>
-					
-					<label for="email"><b>User Email</b></label>
+
+					<label for="employee-lastname"><b>Employee Lastname</b></label>
+					<input type="text" placeholder="Enter Last Name" name="new_employee_lname" required>
+										
+					<label for="employee-email"><b>Employee Email</b></label>
 					<input type="text" placeholder="Enter Email" name="new_employee_email" required>
 
 					<label for="psw"><b>Password</b></label>
@@ -41,9 +80,7 @@
 					<label for="psw-repeat"><b>Repeat Password</b></label>
 					<input type="password" placeholder="Repeat Password" name="psw-repeat" required>
 					
-					<label for="lastname"><b>Employee Lastname</b></label>
-					<input type="text" placeholder="Repeat Password" name="new_employee_lname" required>
-					
+				
 					<label>
 						<input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
 					</label>
