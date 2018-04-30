171ca780f6f638893d01ef63af623a03ab8044f1
odonnellcp <connor.odonnell625@gmail.com>
2018-04-30 10:23:46 -0700

    Updated Delete Shift
    Delete shift should now work with Heroku DB

--- Php/delete_shifts.php
+++ Php/delete_shifts.php
@@ -1,15 +1,16 @@
 <?php
-
 //including the database connection file
 include("initiate_db.php");
- 
+
 //getting id of the data from url
 $shiftid = $_GET['shiftid'];
- 
 //deleting the row from table
-$result = mysqli_query($conn, "DELETE FROM shifts WHERE shiftid=$shiftid");
- 
-//redirecting to the display page (index.php in our case)
-header("Location:create_shifts.php");
+$query = "DELETE FROM created_shifts WHERE shiftid='".$shiftid."'";
+$stmt = $db->prepare($query);
+$stmt->execute();
+//left until proven to work
+// $result = mysqli_query($conn, "DELETE FROM created_shifts WHERE shiftid='".$shiftid."'");
 
+//redirecting to the shift display page
+header("Location:create_shifts.html.php");
 ?>
