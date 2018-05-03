80e90f146c5b50e397e5bd4eaedee865fcb1eda4
odonnellcp <connor.odonnell625@gmail.com>
2018-05-03 10:43:56 -0700

    Updated view_schedule.html.php

--- Php/view_schedule.html.php
+++ Php/view_schedule.html.php
@@ -6,54 +6,52 @@
 <body>
 <?php
 	include("initiate_db.php");
-	
-	echo "<h1>Employee Schedule</h1>";
-	
+
+	echo "<h1>Schedule</h1>";
+
 
 	// Attempt select query execution
 	$sql = "SELECT * FROM final_shifts";
 	if($result = $conn->query($sql)){
 		if($result->num_rows > 0){
-			
-			echo "<table align='center'>";
-            echo "<tr>";
-            echo "<th>First Name</th>";
-            echo "<th>Last Name</th>";
-            echo "<th>Day</th>";
-            echo "<th>Start Time</th>";
-	    echo "<th>End Time</th>";
-	    // echo "<th>Company</th>";		
-            echo "</tr>";
-			while($row = $result->fetch_array()){
-				$empid=$row['username'];
-				$sql2 = "SELECT fname,lname FROM employees WHERE username='$empid'";
-				$result2 = $conn->query($sql2);
-				$empcolumns = $result2->fetch_array();
-				$shiftid = $row['idshift'];
-				$sql3 = "SELECT date,time_start, time_end FROM created_shifts WHERE idshift='$shiftid'";
-				$result3 = $conn->query($sql3);
-				$shiftcolumns = $result3->fetch_array();
-				
+  		echo "<table align='center'>";
+        echo "<tr>";
+          echo "<th>First Name</th>";
+          echo "<th>Last Name</th>";
+          echo "<th>Day</th>";
+          echo "<th>Start Time</th>";
+	        echo "<th>End Time</th>";
+	     // echo "<th>Company</th>";
+        echo "</tr>";
+			  while($row = $result->fetch_array()){
+          $empid=$row['username'];
+				  $sql2 = "SELECT fname,lname FROM employees WHERE username='$empid'";
+				  $result2 = $conn->query($sql2);
+				  $empcolumns = $result2->fetch_array();
+				  $shiftid = $row['idshift'];
+				  $sql3 = "SELECT date,time_start, time_end FROM created_shifts WHERE idshift='$shiftid'";
+				  $result3 = $conn->query($sql3);
+				  $shiftcolumns = $result3->fetch_array();
 				echo "<tr>";
-                echo "<td>". $empcolumns['fname']."</td>";
-                echo "<td>". $empcolumns['lname']."</td>";
-                echo "<td>" . $shiftcolumns['day'] . "</td>";
-                echo "<td>" . $shiftcolumns['time-start'] . "</td>";
-		echo "<td>" . $shiftcolumns['time-end'] . "</td>";
-		echo "</tr>";
+          echo "<td>". $empcolumns['fname']."</td>";
+          echo "<td>". $empcolumns['lname']."</td>";
+          echo "<td>" . $shiftcolumns['day'] . "</td>";
+          echo "<td>" . $shiftcolumns['time-start'] . "</td>";
+		      echo "<td>" . $shiftcolumns['time-end'] . "</td>";
+		    echo "</tr>";
 				//$result2->free();
 				//$result3->free();
-		}
-		echo "</table>";
+		    }
+		  echo "</table>";
 			// Free result set
 			$result->free();
-    } 	else{
+    }else{
 			echo "No records matching your query were found.";
-			}
-	} else{
-		echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
 		}
- 
+	}else{
+		echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+	}
+
 	// Close connection
 	?>
 	</body>
