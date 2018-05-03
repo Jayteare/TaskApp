92e4313ca908762c2b7458d63142fc80a0f9dfe8
raymondlam0128 <raymondlam012892@gmail.com>
2018-05-03 13:30:04 -0700

    Update manager_approval.html.php

--- Php/manager_approval.html.php
+++ Php/manager_approval.html.php
@@ -33,7 +33,7 @@
 
     $mfname = $_SESSION['fname'];
     $mlname = $_SESSION['lname'];
-    $conn = new mysqli("localhost", "root", "", "462_schedule_project");
+    $conn = new mysqli('us-cdbr-iron-east-05.cleardb.net:3306', 'b52e20d0f5da46', 'fc4f25b0', 'heroku_0188da0de4a5cfa');
     if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
     }
