c1a2bd761955593cc0a2172bf0b248db5692f0d1
odonnellcp <connor.odonnell625@gmail.com>
2018-05-03 11:28:47 -0700

    Update view_schedule.html.php

--- Php/view_schedule.html.php
+++ Php/view_schedule.html.php
@@ -8,39 +8,35 @@ sesssion_start();
     <link href="styles.css" media="screen" rel="stylesheet" type="text/css" />
   </head>
   <body>
+	  <h1>Schedule</h1>
 <?php
-	echo "<h1>Schedule</h1>";
-	  // Attempt select query execution
-	$query = "SELECT * FROM final_shifts WHERE company_id = '".$_SESSION['company_id']."'";
+	// Attempt select query execution
+	$query = " SELECT final.idshift, final.fname, final.lname, create.date, create.time_start, create.time_end
+	           FROM final_shifts as final, created_shifts as create
+						 WHERE company_id = '".$_SESSION['company_id']."' and final.idshift = create.idshift ";
   $stmt = $db->prepare($query);
   $stmt->execute();
   $result = $stmt->fetchAll();
-	echo "<table align='center'>";
-		echo "<tr>";
-			echo "<th>First Name</th>";
-			echo "<th>Last Name</th>";
-			echo "<th>Day</th>";
-			echo "<th>Start Time</th>";
-			echo "<th>End Time</th>";
-	 // echo "<th>Company</th>";
-		echo "</tr>";
-	foreach($result as $row){
-	  $shiftid = $row['idshift'];
-		$shiftquery = "SELECT date,time_start, time_end FROM created_shifts WHERE idshift='$shiftid'";
-	  $shiftstmt = $db->prepare($shiftquery);
-	  $shiftstmt->execute();
-	  $shiftresult = $shiftstmt->fetchAll();
-		foreach($shiftresult as $shiftrow){
-		  echo "<tr>";
-        echo "<td>". $row['fname']."</td>";
-        echo "<td>". $row['lname']."</td>";
-        echo "<td>" . $shiftrow['date'] . "</td>";
-        echo "<td>" . $shiftrow['time_start'] . "</td>";
-		    echo "<td>" . $shiftrow['time_end'] . "</td>";
-		  echo "</tr>";
-	  }
-	}
-	echo "</table>";
-	?>
-	  </body>
-	</html>
+?>
+  	<table align='center'>
+      <tr>
+				<th>Shift ID</th>
+    	  <th>First Name</th>
+        <th>Last Name</th>
+      	<th>Day</th>
+    	  <th>Start Time</th>
+    	  <th>End Time</th>
+    	</tr>
+<?php	foreach($result as $row){ ?>
+    	<tr>
+				<td><?php echo $row['final.idshift']?></td>
+        <td><?php echo $row['final.fname']?></td>
+        <td><?php echo $row['final.lname']?></td>
+        <td><?php echo $row['create.date']?></td>
+        <td><?php echo $row['create.time_start']?></td>
+    	  <td><?php echo $row['create.time_end']?></td>
+    	</tr>
+<?php } ?>
+    	</table>
+  </body>
+</html>
