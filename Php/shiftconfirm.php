fd47a24e17629b2d00a6af39b1924f550d5d932a
odonnellcp <connor.odonnell625@gmail.com>
2018-05-04 17:24:16 -0700

    Updated time formatting

--- Php/shiftconfirm.php
+++ Php/shiftconfirm.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   include 'initiate_db.php';
-  $query = "SELECT date, TIME_FORMAT(time_start, '%H:%i') as time_start, TIME_FORMAT(time_end, '%H:%i') as time_end FROM created_shifts WHERE idshift = '".$_SESSION['shift_enroll_id']."' && company_id = '".$_SESSION['company_id']."' ";
+  $query = "SELECT date, TIME_FORMAT(time_start, '%h:%i%p') as time_start, TIME_FORMAT(time_end, '%h:%i%p') as time_end FROM created_shifts WHERE idshift = '".$_SESSION['shift_enroll_id']."' && company_id = '".$_SESSION['company_id']."' ";
   $stmt = $db->prepare($query);
   $stmt->execute();
   $result = $stmt->fetchAll();
