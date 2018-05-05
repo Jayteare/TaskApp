0bc2e6e5900e89bd1a334ad9685fe0555f642d43
odonnellcp <connor.odonnell625@gmail.com>
2018-05-04 17:00:49 -0700

    Updated shift confirm

--- Php/shiftconfirm.php
+++ Php/shiftconfirm.php
@@ -5,7 +5,7 @@
   $query = "SELECT COUNT(*)
             FROM(SELECT *
                  FROM queued_shifts
-                 WHERE idshift = '".$_SESSION['shift_enroll_id']."' && company_id = '".$_SESSION['company_id']."' && username = '".$_SESSION['cur_user']."') ";
+                 WHERE idshift = '".$_SESSION['shift_enroll_id']."' && company_id = '".$_SESSION['company_id']."' && username = '".$_SESSION['cur_user']."') AS x ";
   $stmt = $db->prepare($query);
   $stmt->execute();
   $result = $stmt->fetchAll();
